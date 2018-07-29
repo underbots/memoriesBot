750b19f1eaf733a6bda35980d6234af03a2c986a
Blanca <blancacano2.71828@gmail.com>
2018-07-29 16:48:42 +0200

    git_update

--- git_update.py
+++ git_update.py
@@ -1,8 +1,9 @@
 import os
 
 def git_update(commit , add='.' , path='.'):
-    os.system(f'cp {add} {add}')
+    os.system(f'cp {add} {path}')
     os.system(f'cd {path}')
     os.system(f'git add {add}')
+    os.system('ls')
     os.system(f'git commit -m "{commit}"')
     os.system('git push origin master')
